369902fad43b615b9da9e3b910610d95dbf35d58
1argeD <gkswhdgur1234@naver.com>
2022-12-19 18:45:16 +0900

    2022-12-18{삼총사-코틀린}

--- src/main/java/org/example/TCK/삼총사.kt
+++ src/main/java/org/example/TCK/삼총사.kt
@@ -3,7 +3,7 @@ package org.example.TCK
 class 삼총사 {
     fun solution(number:Array<Int>):Int{
         var answer:Int=0
-        for((i,n) in number.withIndex()) {
+        for((i,n) in number.withIndex()) { /*withIndex()를 사용하면(index, value) 쌍 값을 얻을 수 있다. */
             for((j,m) in number.withIndex()) {
                 for((k,l) in number.withIndex()) {
                     if((i>j)&&(j>k)) {
