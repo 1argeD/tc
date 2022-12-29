5f6c6870ddf3becf343f57629d4f50e2bcb828f8
1argeD <gkswhdgur1234@naver.com>
2022-12-29 17:17:28 +0900

    2022-12-29(명예의전당)

--- src/main/java/org/example/TC/명예의전당.java
+++ src/main/java/org/example/TC/명예의전당.java
@@ -1,7 +1,7 @@
 package org.example.TC;
 
 import java.util.PriorityQueue;
-
+/*https://school.programmers.co.kr/learn/courses/30/lessons/138477?language=java*/
 public class 명예의전당 {
     public int[] solution(int k, int[] score) {
         PriorityQueue<Integer>priorityQueue = new PriorityQueue<>();
@@ -12,10 +12,9 @@ public class 명예의전당 {
             if(priorityQueue.size()>k) {
                 priorityQueue.poll();
             }
-            /*peek()은 스택 위의 가장 위의 element를 반환한다.*/
+            /*peek()은 스택 위의 가장 위의 element 를 반환한다.*/
             answer[i] = priorityQueue.peek();
         }
-
         return answer;
     }
 }
