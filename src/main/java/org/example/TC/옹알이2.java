d5fc8195722b6483724acff51224c7de8d490c94
1argeD <gkswhdgur1234@naver.com>
2023-01-01 17:51:33 +0900

    2023-01-01(옹알이2)

--- src/main/java/org/example/TC/옹알이2.java
+++ src/main/java/org/example/TC/옹알이2.java
@@ -3,6 +3,7 @@ package org.example.TC;
 
 /*https://school.programmers.co.kr/learn/courses/30/lessons/133499*/
 public class 옹알이2 {
+
     public int solution(String[] babbling) {
         int answer = 0;
         String[] words = {"aya", "ye", "woo", "ma"};
