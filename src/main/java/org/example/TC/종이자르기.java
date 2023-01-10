7451c5fb2a43d2c07981c5f6e2b25dd3a10c2acc
1argeD <94172354+1argeD@users.noreply.github.com>
2023-01-10 23:39:25 +0900

    Revert "<feat>택배 배달과 수거"
    This reverts commit 55d6de8893344febad8482f0a92743215d9d966e.

--- src/main/java/org/example/TC/종이자르기.java
+++
@@ -1,9 +0,0 @@
-package org.example.TC;
-/*https://school.programmers.co.kr/learn/courses/30/lessons/120922*/
-public class 종이자르기 {
-    public int solution(int M, int N) {
-        int answer = M*N-1;
-
-        return answer;
-    }
-}
--- src/main/java/org/example/TC/택배배달과수거.java
+++
@@ -1,9 +0,0 @@
-package org.example.TC;
-
-public class 택배배달과수거 {
-    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
-        long answer = -1;
-
-        return answer;
-    }
-}
