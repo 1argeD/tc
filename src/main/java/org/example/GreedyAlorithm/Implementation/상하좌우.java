885f0762ebacbd6a1620ed921c794a31dc94407c
1argeD <94172354+1argeD@users.noreply.github.com>
2023-01-16 23:43:47 +0900

    <feat> Greedy알고리즘 - 상하좌우

--- src/main/java/org/example/GreedyAlorithm/Implementation/상하좌우.java
+++ src/main/java/org/example/GreedyAlorithm/Implementation/상하좌우.java
@@ -1,4 +1,9 @@
 package org.example.GreedyAlorithm.Implementation;
+
+import java.util.Stack;
+
+import static java.util.stream.IntStream.range;
+
 /*여행가 A는 N x N 크기의 정사각형 공간 위에 서 있습니다.
 이 공간은 1 x 1크기의 정사각형으로 나누어져 있습니다.
 가장 왼쪽 위 좌표는 (1, 1)이며, 가장 오른쪽 아래 좌표는 (N, N)에 해당합니다.
@@ -14,4 +19,17 @@ D: 아래쪽으로 한 칸 이동
 이때 여행가 A가 N x N크기의 정사각형 공간을 벗어나는 움직임은 무시됩니다. 예를 들어 (1, 1)의 위치에서 L 혹은 U를 만나면 무시됩니다.
 다음은 N = 5인 지도와 계획서입니다.*/
 public class 상하좌우 {
+    public int[] solution(int n, String[] move ) {
+//        Stack<String> move_point = new Stack<>();
+        int[] answer = {};
+        /*LRUD*/
+        int[] x = {-1,1,0,0};
+        int[] y = {0,0,1,-1};
+        String[]  move_point= {"L","R","U","D"};
+
+        for(range(0, move_point.length);) {
+            }
+        }
+        return answer;
+    }
 }
