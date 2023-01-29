c8d25ea7cdd5e7d24f4fb059a1082e59af1707a5
1argeD <94172354+1argeD@users.noreply.github.com>
2023-01-30 04:03:31 +0900

    <feat> 이모티콘할인행사

--- src/main/java/org/example/lv2/이모티콘할인행사.java
+++ src/main/java/org/example/lv2/이모티콘할인행사.java
@@ -7,28 +7,31 @@ public class 이모티콘할인행사 {
     public int[] solution(int[][] users, int[] emoticons) {
         int[] answer = {};
         int[] sale = {10,20,30,40};
+        int sum = 0;
+        int dex = 0;
+        int cnt = 0;
+
+        int ground =0;
         ArrayList<int[]> per = new ArrayList<>();
 
         for(int i=0; i<users.length; i++){
-            per.add(users[i]);
+            per.add(users[i]);//int[][] 형 int[]로 변환
             int[] s = per.get(i);
-            int sum = 0;
-            int cnt = 0;
-            for(int j=sale.length-1; j>=0;--j){
-                if(s[0]>sale[sale.length-1]){
-                    for(int k=emoticons.length-1;k>=0;--k){
-                        sum += emoticons[k]-emoticons[k]*s[0]/100;
+            for(int j : sale) {
+                if(s[0]<=j){
+                    for (int emoticon : emoticons) {
+                        dex = emoticon - emoticon / 100 * j; //할인율 대로 이모티콘 순서대로 할인하기
+                        sum += dex;  //구매한 임티 값 총합
+                        if(sum>s[1]) {
+                            cnt++;
+                            sum=0;
+                        }
                     }
                 }
             }
-            if(sum>s[1]) {
-                ++cnt;
-                int j = sum;
-
-                answer = new int[]{cnt, j};
+            ground+=sum;
+            answer = new int[] {cnt, ground};
             }
-        }
-
         return answer;
     }
-}
+    }
