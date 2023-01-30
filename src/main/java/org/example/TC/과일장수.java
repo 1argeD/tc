cae35fe46227d94877d83134f0c4866a99c576b1
1argeD <94172354+1argeD@users.noreply.github.com>
2023-01-31 02:24:05 +0900

    <feat> 과일장수

--- src/main/java/org/example/TC/과일장수.java
+++ src/main/java/org/example/TC/과일장수.java
@@ -2,7 +2,7 @@ package org.example.TC;
 
 import java.util.ArrayList;
 import static java.util.Collections.sort;
-
+/*https://school.programmers.co.kr/learn/courses/30/lessons/135808?language=java*/
 public class 과일장수 {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
