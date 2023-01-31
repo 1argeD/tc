23679d152d64fba9c6520de4dcb24173d59b14b0
1argeD <94172354+1argeD@users.noreply.github.com>
2023-02-01 03:08:17 +0900

    <feat> 과일장수

--- src/main/java/org/example/TC/과일장수.java
+++ src/main/java/org/example/TC/과일장수.java
@@ -1,13 +1,11 @@
 package org.example.TC;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 /*https://school.programmers.co.kr/learn/courses/30/lessons/135808?language=java*/
 public class 과일장수 {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        ArrayList<Integer> box = new ArrayList<>();
         Arrays.sort(score);
         for(int i=score.length-1; i>score.length%m; i--) {
             answer+=score[i];
