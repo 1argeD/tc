ce00f925159400e6c48d0a94f833a2bd3b84e7c2
1argeD <94172354+1argeD@users.noreply.github.com>
2023-02-01 17:54:13 +0900

    <feat> 과일장수-시간복잡도 반례 해결

--- src/main/java/org/example/TC/과일장수.java
+++ src/main/java/org/example/TC/과일장수.java
@@ -1,5 +1,6 @@
 package org.example.TC;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 /*https://school.programmers.co.kr/learn/courses/30/lessons/135808?language=java*/
@@ -7,8 +8,18 @@ public class 과일장수 {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
         Arrays.sort(score);
-        for(int i=score.length-1; i>score.length%m; i--) {
-            answer+=score[i];
+        ArrayList<Integer>box = new ArrayList<>();
+        int s = 0;
+        int min = 0;
+        int cnt = 0;
+        for(int i=score.length-1; i>=0; i--) {
+            cnt++;
+            box.add(score[i]);
+            min = Math.min(10,score[i]);
+            if(cnt%m==0) {
+                s=min*m;
+                answer+=s;
+            }
         }
         return answer;
     }
