8c29cee32a12c410266e3eea5b57b34f6e60162a
1argeD <gkswhdgur1234@naver.com>
2023-03-19 22:58:08 +0900

    <chore> 리코쳇 로봇(미완)

--- src/main/java/org/example/lv2/리코쳇로봇.java
+++ src/main/java/org/example/lv2/리코쳇로봇.java
@@ -10,56 +10,15 @@ import java.util.Arrays;
 public class 리코쳇로봇 {
     public int solution(String[] board) {
         int answer = 0;
-        if(success(board).equals("no")) {
-            answer = -1;
-        } else {
-            answer = clear(board);
+        int[] standard = {0, 0};
+        int[] move = {-1, 1, 1, -1};
+        int[] position = {0, board.length};
+        for(String s : board) {
+            int k = s.split("").length;
+            position = new int[] {k, board.length};
         }
+
+
         return answer;
     }
-
-    public String success(String[] board) {
-        String[] answer = {"yes", "no"};
-        String a = "";
-        ArrayList<String> ary = new ArrayList<>(Arrays.asList(board));
-        int g = 0;
-        int l = 0;
-        for (int findG=0; findG<ary.size(); findG++) {
-            if (ary.get(findG).contains("G")) {
-                for (int k = 0; k < ary.get(findG).split("").length; k++) {
-                    g = findG;
-                    if(ary.get(findG).split("")[k].matches("G")) {
-                        l = k;
-                    }
-                    if(g==0) {
-                        return answer[0];
-                    } else if(g==ary.size()-1) {
-                        return answer[0];
-                    } else if(ary.get(findG).split("")[0].matches("G")) {
-                        return answer[0];
-                    } else if(ary.get(findG).split("")[ary.get(findG).split("").length-1].matches("G")) {
-                        return answer[0];
-                    }
-                }
-            }
-        }
-        if(ary.get(g-1).split("")[l].matches("D")) {
-            a = answer[0];
-        } else if(ary.get(g+1).split("")[l].matches("D")) {
-            a = answer[0];
-        } else if(ary.get(g).split("")[l].matches("D")) {
-            a = answer[0];
-        } else {
-            a = answer[1];
-        }
-        return a;
-    }
-
-
-    public int clear(String[] board) {
-        int a = 0;
-        return a;
-    }
-
-
 }
