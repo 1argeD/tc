008b299cf34e124948f1a217933f7c80228254dd
1argeD <gkswhdgur1234@naver.com>
2023-03-27 05:01:20 +0900

    <chore> 백준 피보나치

--- src/main/java/org/example/baekjoon/test/피보나치.java
+++ src/main/java/org/example/baekjoon/test/피보나치.java
@@ -4,14 +4,15 @@ import java.util.Scanner;
 
 public class 피보나치 {
     static class  Main{
+        static Integer[][] dp = new Integer[41][2];
+
         public static void main(String[] args) {
             Scanner sc = new Scanner(System.in);
             int T = sc.nextInt();
             while(T-- > 0) {
                 int n = sc.nextInt();
-                int[] count = {0, 0};
-                fibonacci(n, count);
-                System.out.println(count[0] + " "+ count[1]);
+                fibonacci2(n);
+                System.out.println(dp[n][0] + " "+ dp[n][1]);
             }
         }
 
@@ -25,6 +26,18 @@ public class 피보나치 {
             } else {
                 return fibonacci(n-1, count) + fibonacci(n-2, count);
             }
+        }
+
+        public static Integer[] fibonacci2(int n) {
+            dp[0][0] = 1;
+            dp[1][0] = 0;
+            dp[1][1] = 1;
+            dp[0][1] = 0;
+                if(dp[n][0]==null || dp[n][1]==null) {
+                    dp[n][0] = fibonacci2(n -1)[0] + fibonacci2(n-2)[0];
+                    dp[n][1] = fibonacci2(n-1)[1] + fibonacci2(n-2)[1];
+                }
+        return dp[n];
         }
     }
 }
