cbf372329a1f5aca63034d8c808ae3fef354d945
1argeD <gkswhdgur1234@naver.com>
2023-04-06 03:38:14 +0900

    <feat> 코딩테스트공부

--- src/main/java/org/example/lv3/코딩테스트공부.java
+++ src/main/java/org/example/lv3/코딩테스트공부.java
@@ -13,12 +13,43 @@ public class 코딩테스트공부 {
            goal_c = Math.max(problems[i][1], goal_c);
        }
 
+       if(goal_a<=alp && goal_c<= cop) {
+           return 0;
+       }
+       if(alp>=goal_a) {
+           alp=goal_a;
+       }
+       if(cop>=goal_c) {
+           cop=goal_c;
+       }
+
         int[][] dp = new int [goal_a+2][goal_c+2];
 
-        dp[alp][cop] = 0;
-        int answer = 0;
+       for(int i = alp; i<goal_a; i++) {
+           for(int j = cop; j<goal_c; j++) {
+               dp[i][j] = Integer.MAX_VALUE;
+                dp[i+1][j] = Math.min(dp[i+1][j], dp[i][j]+1);
+                dp[i][j+1] = Math.min(dp[i][j+1], dp[i][j]+1);
 
+               dp[alp][cop] = 0;
 
-        return answer;
+                for(int[] p : problems) {
+                    if(i>=p[0]&&j>=p[1]) {
+                        if(i+p[2]>goal_a&&j+p[3]>goal_c) {
+                            dp[goal_a][goal_c] = Math.min(dp[goal_a][goal_c], dp[i][j]+p[4]);
+                        }
+                        else if(i+p[2]>goal_a) {
+                            dp[goal_a][j+p[3]] = Math.min(dp[goal_a][goal_c], dp[i][j]+p[4]);
+                        } else if(j+p[3]>goal_c) {
+                            dp[i+p[2]][goal_c] = Math.min(dp[i+p[2]][goal_c], dp[i][j]+p[4]);
+                        } else if(i+p[3]>goal_a&&j+p[3]<=goal_c) {
+                            dp[i+p[2]][j+p[3]] =Math.min(dp[i+p[2]][j+p[3]], dp[i][j]+p[4]);
+                        }
+                    }
+                }
+           }
+       }
+
+        return dp[goal_a][goal_c];
     }
 }
---
+++ src/test/java/lv3/코딩테스트공부Test.java
@@ -0,0 +1,20 @@
+package lv3;
+
+import org.assertj.core.api.Assertions;
+import org.example.lv3.코딩테스트공부;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+public class 코딩테스트공부Test {
+    private 코딩테스트공부 coding = new 코딩테스트공부();
+
+    @Test
+    void solution() {
+        int alp = 10;
+        int cop = 10;
+        int[][] problems = {{10,15,2,1,2}, {20,20,3,3,4}};
+        int result = coding.solution(alp, cop, problems);
+        int answer = 15;
+        Assertions.assertThat(answer).isEqualTo(result);
+    }
+}
