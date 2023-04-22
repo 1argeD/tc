973461b83226bcc90e6206be47ad8665f6873973
1argeD <gkswhdgur1234@naver.com>
2023-04-22 16:57:06 +0900

    <chore>요격시스템

--- src/main/java/org/example/lv2/요격시스템.java
+++ src/main/java/org/example/lv2/요격시스템.java
@@ -1,19 +1,22 @@
 package org.example.lv2;
 
-
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Comparator;
 
 /*https://school.programmers.co.kr/learn/courses/30/lessons/181188*/
 public class 요격시스템 {
-    public int solution(int[][] target) {
-        int answer = 0;
-       Arrays.sort(target, ((o1, o2) -> {
-           System.out.println(o1[1] - o2[1]);
-           return o1[1] - o2[1];
-       }));
-        return answer;
+    public int solution(int[][] targets) {
+        Arrays.sort(targets, Comparator.comparingInt(o -> o[1]));
+        int cnt = 0;
+        double last = -1;
+
+        for (int[] t : targets) {
+            if (t[0] > last) {
+                cnt++;
+                last = t[1] - 0.5;
+            }
+        }
+        return cnt;
     }
+
 }
--- src/test/java/lv2/요격시스템Test.java
+++ src/test/java/lv2/요격시스템Test.java
@@ -2,7 +2,6 @@ package lv2;
 
 import org.assertj.core.api.Assertions;
 import org.example.lv2.요격시스템;
-import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.Test;
 
 public class 요격시스템Test {
@@ -10,9 +9,9 @@ public class 요격시스템Test {
 
     @Test
     void solution() {
-        int[][] target = {{4,5}, {4,8}, {10,14}, {11,13}, {5,12}, {3,7}, {1,4}};
+        int[][] targets = {{4,5},{4,8},{10,14},{11,13},{5,12},{3,7},{1,4}};
         int answer = 3;
-        int result = system.solution(target);
+        int result = system.solution(targets);
         Assertions.assertThat(answer).isEqualTo(result);
     }
 }
