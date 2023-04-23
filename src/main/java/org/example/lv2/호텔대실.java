4b6dcf762d64c082db38b60aa25d49ab1ee2dbe4
1argeD <gkswhdgur1234@naver.com>
2023-04-23 18:28:23 +0900

    <feat>호텔대실

--- src/main/java/org/example/lv2/호텔대실.java
+++ src/main/java/org/example/lv2/호텔대실.java
@@ -1,21 +1,27 @@
 package org.example.lv2;
 
+import java.time.LocalTime;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+import static java.util.Arrays.*;
+
 public class 호텔대실 {
     public int solution(String[][] book_time) {
-        int roomCNT = book_time.length;
+        // 시작 시간 기준으로 정렬하되, 시작 시간이 같으면 종료 시간 기준으로 정렬
+        sort(book_time, Comparator.comparing((String[] a) -> a[0]).thenComparing(a -> a[1]));
 
-        for(int i=0; i<book_time.length; i++) {
-            for(int j =0; j<book_time.length; j++) {
-                if(Integer.parseInt(String.valueOf(book_time[i][1].split(":")[0])) < Integer.parseInt(String.valueOf(book_time[j][0].split(":")[0]))) {
-                    roomCNT--;
-                    break;
-                } else if((book_time[i][1].split(":")[0].equals(book_time[j][0].split(":")[0]))) {
-                    if(Integer.parseInt(String.valueOf(book_time[i][1].split(":")[1]))+10<=Integer.parseInt(String.valueOf(book_time[j][0].split(":")[1]))) {
-                        roomCNT--;
-                    }
-                }
+        PriorityQueue<LocalTime> checkoutTimes = new PriorityQueue<>();
+
+        for (String[] reservation : book_time) {
+            LocalTime checkIn = LocalTime.parse(reservation[0]);
+            LocalTime checkOut = LocalTime.parse(reservation[1]).plusMinutes(10);
+            if (!checkoutTimes.isEmpty() && !checkIn.isBefore(checkoutTimes.peek())) {
+                checkoutTimes.poll();
             }
+            checkoutTimes.add(checkOut);
         }
-        return roomCNT;
+
+        return checkoutTimes.size();
     }
 }
