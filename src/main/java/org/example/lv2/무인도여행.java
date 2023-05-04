e73f69a8f1c483f71c50ba2a39caf27480899d11
1argeD <gkswhdgur1234@naver.com>
2023-05-05 04:51:39 +0900

    <feat>무인도 여행

--- src/main/java/org/example/lv2/무인도여행.java
+++ src/main/java/org/example/lv2/무인도여행.java
@@ -1,63 +1,31 @@
 package org.example.lv2;
 
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.*;
 
 public class 무인도여행 {
-    class Island {
-        int x, y;
-        int[] sum;
+  public int[] solution(String[] maps) {
+      LinkedList<Integer>island = new LinkedList<>();
+      boolean[][] visited = new boolean[maps.length][maps[0].length()];
 
-        public Island(int x, int y, int[] sum) {
-            this.x = x;
-            this.y = y;
-            this.sum = sum;
-        }
-    }
-    public int[] solution(String[] maps) {
-        Island land = null;
-        int[][] move = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+      for(int i=0; i< maps.length; i++) {
+          int sum = 0;
+          for(int j=0; j<maps[0].length();j++) {
+              sum=search(i,j,visited, maps);
+              if(sum>0) island.add(sum);
+          }
+      }
 
-        for (int y = 0; y < maps.length; y++) {
-            for (int x = 0; x < maps[0].length(); x++) {
-                if (maps[y].charAt(x) != 'X') {
-                    int sum = Integer.parseInt(String.valueOf(maps[y].charAt(x)));
-                    land = new Island(x, y, new int[] {sum});
-                }
-            }
-        }
+      Collections.sort(island);
+      return island.isEmpty()?new int[]{-1}:island.stream().mapToInt(Integer::intValue).toArray();
+  }
 
-        Queue<Island> pointQueue = new ArrayDeque<>();
-        boolean[][] visited = new boolean[maps.length][maps[0].length()];
-        pointQueue.add(land);
-
-        while (!pointQueue.isEmpty()) {
-            Island island = pointQueue.poll();
-            int x = island.x, y = island.y;
-            int[] sum = island.sum;
-
-            if (visited[x][y]) continue;
-            visited[x][y] = true;
-
-            if (x == land.x && y == land.y && sum== land.sum) return sum;
-
-            for(int[] position : move) {
-                int xPoint = x, yPoint = y;
-                int[] sum1 = sum;
-                while (0<=xPoint+position[0]&&
-                        xPoint+position[0]<maps[0].length() &&
-                        0<=yPoint+position[1]&&
-                        yPoint+position[1]<maps.length&&
-                        maps[xPoint+position[0]].charAt(yPoint+position[1])!='X') {
-                    xPoint += position[0];
-                    yPoint += position[1];
-                    sum1.clone();
-                }
-                if(!visited[xPoint][yPoint]) pointQueue.add(new Island(xPoint, yPoint, sum1));
-            }
-
-        }
-
-        return new int[]{-1};
-    }
+  public int search(int i, int j, boolean[][] visited, String[] maps) {
+      if(i<0||j<0||i>=visited.length||j>=visited[0].length|| visited[i][j] ||maps[i].charAt(j)=='X') return 0;
+      visited[i][j]=true;
+      return (maps[i].charAt(j)-'0')
+              + search(i-1,j,visited,maps)
+              + search(i+1,j,visited,maps)
+              + search(i,j-1,visited,maps)
+              + search(i,j+1,visited,maps);
+  }
 }
