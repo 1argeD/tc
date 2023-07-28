b2aaaf723eab99f7675f56f42e223446a5215e7a
HJH <gkswhdgur1234@naver.com>
2023-07-28 17:07:59 +0900

    <chore>미로탈출명령어

--- src/main/java/org/example/lv3/미로탈출명령어.java
+++ src/main/java/org/example/lv3/미로탈출명령어.java
@@ -1,18 +1,17 @@
 package org.example.lv3;
 
+import java.util.ArrayDeque;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class 미로탈출명령어 {
     public class Postion {
         int a,b,cnt;
-        char SG;
         String pattern;
-        Postion(int a, int b, int cnt, char SG, String pattern) {
+        Postion(int a, int b, int cnt, String pattern) {
             this.a = a;
             this.cnt = cnt;
             this.b = b;
-            this.SG = SG;
             this.pattern=pattern;
         }
     }
@@ -20,49 +19,53 @@ public class 미로탈출명령어 {
 
         int[][] movePoint = {{1,0},{-1,0},{0,1},{0,-1}};
 
-        boolean[][] filed = new boolean[n][m];
+        boolean[][] field = new boolean[n][m];
 
-        Postion start = new Postion(x-1,y-1,k,'S',"");
-        Postion end = new Postion(r-1,c-1,0,'G',"");
+        Postion start = new Postion(x-1,y-1,0,"");
+        Postion end = new Postion(r-1,c-1,k,"");
 
-        Queue<Postion> postionQueue = new PriorityQueue<>((o1,o2)-> o1.pattern.compareTo(o2.pattern));
-        postionQueue.add(start);
+        field[end.a][end.b]=true;
+
+        PriorityQueue<Postion> postionQueue = new PriorityQueue<>(((o1, o2) -> o1.pattern.compareTo(o2.pattern)));
+        postionQueue.offer(start);
+
 
         while(!postionQueue.isEmpty()) {
-            Postion postion = postionQueue.poll();
-            int positionX = postion.a; int positionY = postion.b; int CNT = postion.cnt; char poitionSG = postion.SG; String pattern =postion.pattern;
 
-            filed[positionX][positionY] = true;
+            Postion position = postionQueue.poll();
 
-            if(positionX==end.a && positionY==end.b && CNT==0) {
-                return pattern;
-            }
+            int queueX = position.a; int queueY = position.b; int CNT = position.cnt; StringBuilder pattern = new StringBuilder(position.pattern);
+            if(field[queueX][queueY] && CNT==k) return pattern.toString();
+            if(CNT > k) continue;
 
+            if(!impossible(queueX,queueY,end.a,end.b,k-CNT)) continue;
 
-            if(!impossible(positionX,positionY,r-1,c-1,k)) continue;
+            for(int[] move : movePoint) {
+                int X = queueX, Y = queueY, count = pattern.length();
+                String answer = pattern.toString();
 
-            for(int[] moveP : movePoint) {
-                int xPoint = positionX, yPoint = positionY; int pointCNT =postion.cnt; char sg = poitionSG;
-                String answer = pattern;
+                while (
+                        count != k &&
+                        X+move[0]>0 &&
+                        Y+move[1]>0 &&
+                        X+move[0]<n &&
+                        Y+move[1]<m
+                )
+                {
+                    if(X < 0 || Y < 0 || X>=n || Y >= m) continue;
 
-                while(xPoint+moveP[0]>=0&&
-                        yPoint+moveP[1]>=0&&
-                        xPoint+moveP[0]<filed.length&&
-                        yPoint+moveP[1]<filed[0].length&&
-                        end.SG!=sg
-                ) {
-                    xPoint+=moveP[0];
-                    yPoint+=moveP[1];
+                    X+=move[0];
+                    Y+=move[1];
 
-                    if(xPoint+moveP[0]>xPoint) {pointCNT--; answer = pattern+"l";}
-                    else if(yPoint+moveP[1]>yPoint){ pointCNT--;answer = pattern+"u";}
-                    else if(xPoint+moveP[0]<xPoint) {pointCNT--; answer = pattern+"r";}
-                    else if(yPoint+moveP[1]<yPoint){ pointCNT--; answer = pattern+"d";}
-                    else if(xPoint+moveP[0]==end.a&&yPoint+moveP[0]==end.b) {
-                        sg='G';
-                    }
+                    ++count;
+
+                    if(move[0]>0) { pattern.append("r");}
+                    else if(move[0]<0) { pattern.append("l");}
+                    else if(move[1]>0) { pattern.append("d");}
+                    else if(move[1]<0) { pattern.append("u");}
+                    answer=pattern.toString();
+                    postionQueue.offer(new Postion(X, Y, count,answer));
                 }
-                if(!filed[xPoint][yPoint]) postionQueue.add(new Postion(xPoint, yPoint, pointCNT, sg, answer));
             }
         }
         return "impossible";
@@ -70,10 +73,12 @@ public class 미로탈출명령어 {
 
     boolean impossible(int x, int y, int r, int c, int k) {
         int rest = Math.abs(r-x)+Math.abs(c-y);
-        if((rest - k) % 2 == 0) return true;
+        if((rest % 2) - (k % 2) == 0) return true;
         if(rest>k) return false;
         return false;
     }
+
+
 
 
 }
