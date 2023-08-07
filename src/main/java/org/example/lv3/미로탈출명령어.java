af9ba404a02a32380744aaf029fcca95f5689c6e
1argeD <gkswhdgur1234@naver.com>
2023-08-07 21:23:18 +0900

    <feat>굳이 스트링 버퍼를 사용할 필요가 없음

--- src/main/java/org/example/lv3/미로탈출명령어.java
+++ src/main/java/org/example/lv3/미로탈출명령어.java
@@ -1,79 +1,71 @@
 package org.example.lv3;
 
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
 import java.util.PriorityQueue;
 
+/*https://school.programmers.co.kr/learn/courses/30/lessons/150365*/
+public class 미로탈출명령어 {
 
-public class 미로탈출명령어 {    public class Position {
-    int a,b,cnt;
-    StringBuilder pattern;
-    Position(int a, int b, int cnt, StringBuilder pattern)  {
-        this.a = a;
-        this.cnt = cnt;
-        this.b = b;
-        this.pattern=pattern;
+    class Position {
+        int a, b, count;
+        String route;
+
+        public Position(int a, int b, int count, String route) {
+            this.a = a;
+            this.b = b;
+            this.count = count;
+            this.route = route;
+        }
     }
+    public String solution(int n, int m, int x, int y, int r, int c, int k ) {
 
-}
+                int[][] movePoint = {{1,0},{-1,0},{0,1},{0,-1}};
 
-    public String solution(int n,int m, int x, int y, int r, int c,int k){
+                Position start = new Position(x-1, y-1,0, "");
+                Position end = new Position(r-1,c-1,k,"dllrl");
 
-        int[][] movePoint = {{1,0},{-1,0},{0,1},{0,-1}};
+        PriorityQueue<Position> priorityPosition = new PriorityQueue<>(Comparator.comparing(o->o.route));
+        priorityPosition.add(start);
 
-        Position start = new Position(x-1,y-1,0,new StringBuilder());
-        Position end = new Position(r-1,c-1,k,new StringBuilder());
+        while (!priorityPosition.isEmpty()) {
+            Position position = priorityPosition.poll();
+            int X = position.a; int Y = position.b; int CNT = position.count; String answer = position.route;
 
-        PriorityQueue<Position> positionQueue = new PriorityQueue<>((Comparator.comparing(o -> o.pattern)));
-        positionQueue.offer(start);
+            if(X == end.a && Y == end.b && CNT == k) { return answer;}
 
-        while(!positionQueue.isEmpty()) {
-            Position position = positionQueue.poll();
-            int queueX = position.a; int queueY = position.b; int CNT = position.cnt; StringBuilder pattern = new StringBuilder(position.pattern);
+            if(!possibleOrImpossible(X,Y,end.a,end.b,k-CNT)) continue;
 
-            if(end.a==queueX&&end.b==queueY&&CNT==k) return pattern.toString();
+            for(int[] move :  movePoint) {
+                int rl = X, ud = Y, count=answer.length(); String ANSWER=answer;
+                if(
+                        rl+move[0]>=0&&
+                        ud+move[1]>=0&&
+                        n>rl+move[0] &&
+                        m>ud+move[1] &&
+                        count<=k
+                    ) {
 
-            if(CNT > k) continue;
+                    rl+=move[0];
+                    ud+=move[1];
 
-            if(!impossible(queueX,queueY,end.a,end.b,k-CNT)) continue;
+                    if(move[0]>0) { ANSWER+="d";}
+                    else if (move[1]>0) { ANSWER+="r";}
+                    else if(move[0]<0) { ANSWER+="u";}
+                    else if(move[1]<0) { ANSWER+="l";}
 
-            for(int[] move : movePoint) {
-                int X = queueX, Y = queueY, count=0;
-                StringBuilder answer;
+                    count = answer.length()+1;
 
-                if (
-                        count != k &&
-                                X+move[0]>=0 &&
-                                Y+move[1]>=0 &&
-                                X+move[0]<n &&
-                                Y+move[1]<m
-                )
-                {
-                    if(X < 0 || Y < 0 || X>=n || Y >= m) continue;
+                    priorityPosition.add(new Position(rl , ud, count, ANSWER));
 
-                    X+=move[0];
-                    Y+=move[1];
-
-                    pattern = new StringBuilder(position.pattern);
-
-                    if(move[0]>0) {  pattern.append("d"); }
-                    else if(move[0]<0) {  pattern.append("u"); }
-                    else if(move[1]>0) {  pattern.append("r"); }
-                    else if(move[1]<0) {  pattern.append("l"); }
-                    count = pattern.length();
-                    answer = pattern;
-                    positionQueue.offer(new Position(X, Y, count, answer));
                 }
             }
+
         }
+
         return "impossible";
     }
-
-    boolean impossible(int x, int y, int r, int c, int k) {
-        int rest = Math.abs(r - x) + Math.abs(c - y);
-        return (rest % 2) - (k % 2) == 0 && rest <= k;
+    public boolean possibleOrImpossible(int x, int y, int r, int c, int k) {
+        int rest = Math.abs(r-x)+Math.abs(c-y);
+        return (rest%2)-(k%2)  == 0&&rest<=k;
     }
-
-
 }
